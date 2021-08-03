6d51911324a50670f61ebf46ff3c263b2866083a
Elise Valkanas <evalkanas@gmail.com>
2021-08-03 14:20:46 -0400

    pseudo code for relatedness

---
+++ run_relatedness.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+#input file of sampleID, gvcf location, seq type
+
+# read in list of gVCFs and
+
+#for each gvcf
+# get sample ID
+
+# reheader to include seq type in sample ID
+
+# subset to purcell 5k sites
+
+# combine all gVCFs in file (regardless of sequencing) 
+
+# Run relatedness using vcftools
+
+# identify duplicate samples 
+
